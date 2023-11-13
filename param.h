0c9e5de94dc95b130ab2735f0c8de7ca27218e96
agreeee <150682757+agreeee@users.noreply.github.com>
2023-11-13 21:04:32 +0800

    Add files via upload

---
+++ param.h
@@ -0,0 +1,9 @@
+#define n 4  //全局属性个数
+#define N 3    //DNF中子句数量
+#define dt 2    //DNF中子句的变量个数
+#define DT 4   //pow(2, dt)
+#define CNT 6    //组合数Cnt
+#define CDT 24   //密钥列表长度= CNT * pow(2, dt)
+#define wt 4     //f中的属性个数
+#define WT 4     //pow(2, w - dt)
+#define L 24   //  (CNT-N) * pow(2, dt) + N * pow(2, w - dt)
